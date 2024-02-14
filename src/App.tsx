35cfeecf60b70fc1b98e8d6f792cb2a31194071a
Carlos Gámez <hardpx@gmail.com>
2024-02-14 10:57:05 -0600

    setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import MuiTypography from './components/MuiTypography';
+
+const App = () => {
+  return (
+    <>
+      <MuiTypography />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,30 @@
+import { Typography } from '@mui/material';
+
+const MuiTypography = () => {
+  return (
+    <>
+      <Typography variant='h1'>h1 Heading</Typography>
+      <Typography variant='h2'>h2 Heading</Typography>
+      <Typography variant='h3'>h3 Heading</Typography>
+      <Typography variant='h4'>h4 Heading</Typography>
+      <Typography variant='h5'>h5 Heading</Typography>
+      <Typography variant='h6'>h6 Heading</Typography>
+
+      <Typography variant='subtitle1'>Subtitle 1</Typography>
+      <Typography variant='subtitle2'>Subtitle 2</Typography>
+
+      <Typography variant='body1'>Body 1</Typography>
+      <Typography variant='body2'>Body 2</Typography>
+
+      <Typography variant='h4' component='h1' gutterBottom>
+        h4 Heading as h1 component with margin on bottom
+      </Typography>
+
+      <Typography variant='h4' component='h1'>
+        h4 Heading as h1 component{' '}
+      </Typography>
+    </>
+  );
+};
+
+export default MuiTypography;
